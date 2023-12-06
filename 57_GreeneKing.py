6cdaf37635aebc02bf00b6923d2228921519eebe
mucc001 <jiamengsunh@outlook.com>
2023-12-06 20:35:18 +0000

    fix: Greene King

--- 57_GreeneKing.py
+++ 57_GreeneKing.py
@@ -5,10 +5,18 @@ import requests
 from define_collection_wave import folder
 from helpers import create_folder, PDFDownloader
 
-fg_path = create_folder('51_FarmhouseInns', folder)
+fg_path = create_folder('57_GreeneKing', folder)
 
 # url found at https://www.greeneking.co.uk/pubs/greater-london/silver-cross/menu
-url_fordownload = 'https://gkbr-p-001.sitecorecontenthub.cloud/api/public/content/79cdd03c480f4c4e855af2cc9e21ae72?v=f3ecf597'
-print("Downloading:", url_fordownload)
-filePath = fg_path + '/' + url_fordownload.split('/')[-1]
-PDFDownloader(url_fordownload, filePath=filePath)
+urls_fordownload = [
+    'https://gkbr-p-001.sitecorecontenthub.cloud/api/public/content/f414e3b9a77842e48de9c7aa19f0d68c?v=7377cce6',
+    'https://gkbr-p-001.sitecorecontenthub.cloud/api/public/content/681e855a3610418db6b3b6409d5d78e2?v=70e2e7f7',
+    'https://gkbr-p-001.sitecorecontenthub.cloud/api/public/content/1150617f977541aab011459ed035a20b?v=d43c9bf0',
+    'https://gkbr-p-001.sitecorecontenthub.cloud/api/public/content/2ecff6ccd8594977a8dbc89e8ee74f1f?v=f35217d8',
+    'https://gkbr-p-001.sitecorecontenthub.cloud/api/public/content/6ebdcf11763c441f8bc98e39971b5d66?v=770e0c2b'
+]
+
+for url in urls_fordownload:
+    print("Downloading:", url)
+    filePath = fg_path + '/' + url.split('/')[-1]
+    PDFDownloader(url, filePath=filePath)
