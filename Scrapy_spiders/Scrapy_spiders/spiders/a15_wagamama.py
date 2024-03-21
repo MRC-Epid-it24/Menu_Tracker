62cc127075bb75e3921588cc4396de18910e698e
mucc001 <jiamengsunh@outlook.com>
2024-03-21 11:32:37 +0000

    fix(wagamama): rewrite the script for the new website layout

--- Scrapy_spiders/Scrapy_spiders/spiders/a15_wagamama.py
+++ Scrapy_spiders/Scrapy_spiders/spiders/a15_wagamama.py
@@ -6,53 +6,69 @@ from browserPath import web_browser_path
 from scrapy import Selector
 from selenium import webdriver
 from selenium.webdriver.common.by import By
+from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.support import expected_conditions as EC
+from selenium.common.exceptions import TimeoutException
 
 
 class A15WagamamaSpider(scrapy.Spider):
     name = '15_Wagamama'
     allowed_domains = ['www.wagamama.com']
-    # start_urls = ['https://www.wagamama.com/our-menu']
     start_urls = ['https://www.wagamama.com/menu?category=sides-sharing']
 
     def __init__(self):
         self.driver = webdriver.Chrome(web_browser_path)
+        self.data = []
 
     def parse(self, response):
         self.driver.get(response.url)
-        sleep(20)
-        # accept cookies
-        # self.driver.find_element(by=By.XPATH, value='//button[@id="onetrust-accept-btn-handler"]').click()
 
-        while len(self.driver.find_elements(by=By.XPATH, value='//li[@data-status="lazy"]')) > 0:
-            toclick = self.driver.find_element(by=By.XPATH, value='//li[@data-status="lazy"]')
-            self.driver.execute_script("arguments[0].click();", toclick)
-            sleep(10)
+        # try:
+        #     WebDriverWait(self.driver, 10).until(EC.element_to_be_clickable((By.XPATH, '//*[@id="onetrust-accept-btn-handler"]'))).click()
+        # except TimeoutException:
+        #     self.log("The cookie button was not clickable within 10 seconds or not present.")
+    
+        category_buttons = WebDriverWait(self.driver, 10).until(EC.presence_of_all_elements_located((By.XPATH, '//button[contains(@class, "_menuItemButton_1etan_2")]')))
+        # category_buttons = self.driver.find_elements(By.XPATH, '//button[contains(@class, "_menuItemButton_1etan_2")]')
+        for button in category_buttons:
+            self.driver.execute_script("arguments[0].click();", button)
+            sleep(1)  # Wait for the content to load
+           
+            food_buttons = self.driver.find_elements(By.XPATH, '//div[contains(@class, "_bottom_pqbyt_34")]//button[contains(@class, "_customButton_dzbl6_2")]')
+            for food_button in food_buttons:
+                try:
+                    WebDriverWait(self.driver, 10).until(EC.element_to_be_clickable(food_button))
+                    self.driver.execute_script("arguments[0].click();", food_button)
+                    sleep(1)  # Wait for modal to open
+                    item_description = driver.find_elements(By.CLASS_NAME, '_description_1nw6t_10').text
+                    table_rows = driver.find_elements(By.XPATH, '//table//tr')
+                    allergens = driver.find_element(By.CLASS_NAME, '_allergens_1741a_15').text
+                    for row in table_rows:
+                        cells = row.find_elements(By.TAG_NAME, 'td')
+                        print("Cells:", cells)
+                        nutrient = cells[0].text
+                        if cells:
+                            self.data = self.data.append({
+                                'rest_name': 'Wagamama',
+                                'collection_date': date.today().strftime("%b-%d-%Y"),
+                                'item_name': food_button.text,
+                                'item_description': item_description,
+                                'allergens': allergens,
+                                nutrient: cells[1].text,
+                                nutrient + '_100': cells[2].text,
+                            })         
 
-        # categories = self.driver.find_elements_by_xpath('//ul[@class="k10-course-selector__wrapper-l1"]/li/span[@class="k10-course-selector__name"]')
-        # for category in categories:
-        #     cat_name = category.find_element_by_xpath('./span').text
-        #     if cat_name is not None:
-        #         category.click()
+                    try: 
+                        # Close the modal to return to the menu
+                        close_button = driver.find_element(By.XPATH, '//button[@id="close"]')
+                        close_button.click()
+                    except Exception as e:
+                        self.log(f"Failed to close modal: {e}")
+                        continue
 
-        resp = Selector(text=self.driver.page_source)
+                except Exception as e:
+                    self.log(f"Error navigating to item details: {e}")
+                    continue
         self.driver.quit()
-        products = resp.xpath('//div[@class="modal-content"]')
-        for product in products:
-            print("product info: ", product, '\n')
-            nutrient_dict = {}
-            rows = product.xpath('.//tr[@class="k10-recipe-modal__tr"]')
-            for row in rows:
-                row_values = row.xpath('./td/text()').getall()
-                print("row_values: ", row_values, "\n")
-                nutrient = row_values[0]
-                perserving = row_values[1]
-                density = row_values[2]
-                nutrient_dict.update({nutrient: perserving, nutrient + '_100': density})
-            product_dict = {
-                'rest_name': 'Wagamama',
-                'collection_date': date.today().strftime("%b-%d-%Y"),
-                'item_name': product.xpath('.//div[@class="k10-recipe-modal__name"]/text()').get(),
-                'item_description': product.xpath('.//div[@class="k10-recipe-modal__desc"]/text()').get(),
-                'allergens': product.xpath('normalize-space(.//div[@class="k10-recipe-modal__labels"]/text())').get()}
-            product_dict.update(nutrient_dict)
-            yield product_dict
+        for item in self.data:
+            yield item
--- define_collection_wave.py
+++ define_collection_wave.py
@@ -2,7 +2,7 @@ import os
 
 # start the collection - create a new master folder
 if __name__ == "__main__":
-    folder = '/Users/jamie/December_collection_2023'
+    folder = '/Users/jamie/March_collection_2024'
     os.mkdir(folder)
 else:
-    folder = '/Users/jamie/December_collection_2023'
+    folder = '/Users/jamie/March_collection_2024'
